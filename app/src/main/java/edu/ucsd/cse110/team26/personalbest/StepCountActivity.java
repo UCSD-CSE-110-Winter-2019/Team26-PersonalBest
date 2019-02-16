0b351a7be7f72cf4299417df03f51a34ef19e1e7
Tram Nguyen <thn099@ucsd.edu>
2019-02-15 19:05:13 -0800

    add line chart

--- app/src/main/java/edu/ucsd/cse110/team26/personalbest/StepCountActivity.java
+++ app/src/main/java/edu/ucsd/cse110/team26/personalbest/StepCountActivity.java
@@ -3,6 +3,7 @@ package edu.ucsd.cse110.team26.personalbest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -25,12 +26,25 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.Locale;
+
+// reference: https://www.studytutorial.in/android-combined-line-and-bar-chart-using-mpandroid-library-android-tutorial
 
 public class StepCountActivity extends AppCompatActivity {
 
@@ -84,38 +98,63 @@ public class StepCountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_count);
 
-        BarChart chart = (BarChart) findViewById(R.id.chart);
-//BarData data = new BarData(getXAxisValues(), getDataSet());
-        ArrayList<BarEntry> BarEntry = new ArrayList<>();
-        BarEntry.add(new BarEntry(0f, new float[] {1, 2}));
-        BarEntry.add(new BarEntry(1f, new float[] {3, 4}));
-        BarEntry.add(new BarEntry(2f, new float[] {1, 4}));
-        BarEntry.add(new BarEntry(3f, new float[] {5, 2}));
-        BarEntry.add(new BarEntry(4f, new float[] {6, 2}));
-        BarEntry.add(new BarEntry(5f, new float[] {1, 3}));
-        BarEntry.add(new BarEntry(6f, new float[] {1, 4}));
+        CombinedChart mChart = (CombinedChart) findViewById(R.id.chart1);
+        mChart.setDrawGridBackground(false);
+        mChart.getDescription().setText("");
+        mChart.setHighlightFullBarEnabled(false);
+        mChart.setDrawOrder(new CombinedChart.DrawOrder[]{
+                CombinedChart.DrawOrder.BAR,  CombinedChart.DrawOrder.LINE
+        });
 
-        BarDataSet dataSet = new BarDataSet(BarEntry, "Step Count");
+        mChart.setDrawBarShadow(false);
+        mChart.setHighlightFullBarEnabled(false);
+
+        mChart.getAxisRight().setDrawGridLines(false);
+        mChart.getAxisLeft().setDrawGridLines(false);
+        mChart.getAxisLeft().setAxisMinimum(0.0f); // this replaces setStartAtZero(true)
+        mChart.getAxisRight().setAxisMinimum(0.0f);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries = getBarEnteries(entries);
+
+        BarDataSet dataSet = new BarDataSet(entries, "Step Count");
         dataSet.setStackLabels(new String[] {"Intentional Walks", "Unintentional Walks"});
-        ArrayList<String> labels = new ArrayList<>();
 
-        labels.add("Mon");
-        labels.add("Tues");
-        labels.add("Wed");
-        labels.add("Thurs");
-        labels.add("Fri");
-        labels.add("Sat");
-        labels.add("Sun");
+        final String[] labels = new String[] {
+                "Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"
+        };
 
+        dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        float barWidth = 0.45f; // x2 dataset
+        BarData d = new BarData(dataSet);
+        d.setBarWidth(barWidth);
         BarData data = new BarData(dataSet);
 
+        mChart.getXAxis().setLabelCount(dataSet.getEntryCount());
 
-        chart.getXAxis().setLabelCount(dataSet.getEntryCount());
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+        xAxis.setAxisMinimum(0f);
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return labels[(int) value % labels.length];
+            }
+        });
 
-        chart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(labels));
-        dataSet.setColors(new int[] {ContextCompat.getColor(chart.getContext(), R.color.colorAccent),
-                ContextCompat.getColor(chart.getContext(), R.color.colorPrimary),});
-        chart.setData(data);
+        dataSet.setColors(new int[] {ContextCompat.getColor(mChart.getContext(), R.color.colorAccent),
+                ContextCompat.getColor(mChart.getContext(), R.color.colorPrimary),});
+
+        CombinedData dataCombined = new CombinedData();
+        dataCombined.setData( generateLineData());
+        dataCombined.setData(data);
+
+        mChart.getXAxis().setAxisMaximum(data.getXMax() + 0.25f);
+        mChart.getXAxis().setAxisMinimum(data.getXMin() - 0.25f);
+        mChart.setData(dataCombined);
+
 
         textSteps = findViewById(R.id.textSteps);
 
@@ -226,5 +265,52 @@ public class StepCountActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private ArrayList<Entry> getLineEntriesData(ArrayList<Entry> entries){
+        entries.add(new Entry(0, 10));
+        entries.add(new Entry(1, 20));
+        entries.add(new Entry(2, 20));
+        entries.add(new Entry(3, 18));
+        entries.add(new Entry(4, 20));
+        entries.add(new Entry(5, 15));
+        entries.add(new Entry(6, 20));
+
+        return entries;
+    }
+
+    private LineData generateLineData() {
+
+        LineData d = new LineData();
+
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+
+        entries = getLineEntriesData(entries);
+
+        LineDataSet set = new LineDataSet(entries, "Goal");
+        set.setLineWidth(2.5f);
+        set.setCircleColor(Color.rgb(60, 79, 109));
+        set.setCircleRadius(5f);
+        set.setFillColor(Color.rgb(60, 79, 109));
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setDrawValues(true);
+        set.setValueTextSize(10f);
+        set.setValueTextColor(Color.rgb(60, 79, 109));
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        d.addDataSet(set);
+
+        return d;
+    }
+
+    private ArrayList<BarEntry> getBarEnteries(ArrayList<BarEntry> entries){
+        entries.add(new BarEntry(0f, new float[] {1, 2}));
+        entries.add(new BarEntry(1f, new float[] {3, 4}));
+        entries.add(new BarEntry(2f, new float[] {1, 4}));
+        entries.add(new BarEntry(3f, new float[] {5, 2}));
+        entries.add(new BarEntry(4f, new float[] {6, 2}));
+        entries.add(new BarEntry(5f, new float[] {1, 3}));
+        entries.add(new BarEntry(6f, new float[] {1, 4}));
+        return  entries;
+    }
 }
+
 
