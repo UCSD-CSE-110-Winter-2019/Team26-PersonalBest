6fc9aac9ad578866f27cfaa2c06bdd5f79e6e36b
LilArveen <31741243+LilArveen@users.noreply.github.com>
2019-03-12 23:50:08 -0700

    Added Unit Tests For Notification; Will discuss UI tests with TA

---
+++ app/src/test/java/edu/ucsd/cse110/team26/personalbest/NotififcationsTest.java
@@ -0,0 +1,90 @@
+package edu.ucsd.cse110.team26.personalbest;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.widget.TextView;
+
+import com.google.gson.annotations.Until;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.shadows.ShadowNotification;
+import org.robolectric.shadows.ShadowNotificationManager;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.robolectric.Shadows.shadowOf;
+
+@RunWith(RobolectricTestRunner.class)
+
+public class NotififcationsTest {
+    private static final String TEST_SERVICE = "TEST_SERVICE";
+
+    private StepCountActivity activity;
+    private TextView textSteps;
+
+
+    @Before
+    public void setUp() throws Exception {
+        Intent intent = new Intent(RuntimeEnvironment.application, StepCountActivity.class);
+        intent.putExtra("DEBUG", true);
+        activity = Robolectric.buildActivity(StepCountActivity.class, intent).create().get();
+
+
+        textSteps = activity.findViewById(R.id.textSteps);
+    }
+
+
+    @Test
+    public void testNotificationBeforeCompletedGoal() {
+        activity.initializeNewDay();
+        Settings settings = new Settings(InstrumentationRegistry.getInstrumentation().getContext(), new ConcreteTimeStamper());
+        settings.saveGoal(2000);
+        activity.setStepCount(1000);
+
+        NotificationManager notificationService = (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
+        ShadowNotificationManager shadowNotificationManager = shadowOf(notificationService);
+        assertEquals(0, shadowNotificationManager.size());
+
+        activity.setStepCount(1999);
+
+        assertEquals(0, shadowNotificationManager.size());
+
+
+    }
+
+    @Test
+    public void testNotificationShowsAfterCompletedGoal() {
+        activity.initializeNewDay();
+        Settings settings = new Settings(InstrumentationRegistry.getInstrumentation().getContext(), new ConcreteTimeStamper());
+        settings.saveGoal(2000);
+        activity.setStepCount(1000);
+
+        NotificationManager notificationService = (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
+        ShadowNotificationManager shadowNotificationManager = shadowOf(notificationService);
+
+        assertEquals(0, shadowNotificationManager.size());
+        settings.saveGoal(2000);
+        activity.setStepCount(2000);
+
+
+        assertEquals(1, shadowNotificationManager.size());
+
+    }
+
+
+
+
+
+
+}
